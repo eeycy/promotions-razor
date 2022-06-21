39859c6ca654d1ab11915b1628d587201bc09f92
Christodoulos Ioannou <cioannou@eey.gov.cy>
2022-06-21 14:50:26 +0300

    Updated to look and feel like govcy template. Added some test components. Added second page and test ref

---
+++ Pages/NewApplication.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace promotions_razor.Pages
+{
+    public class NewApplicationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
